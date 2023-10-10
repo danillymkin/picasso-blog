cf92b58e6c23c2d812c48cb1c389510223bb2ce8
danillyamkin <danilwebdev@gmail.com>
2023-10-10 17:32:08 +0300

    feat: create post page

--- src/pages/post/post-page.tsx
+++ src/pages/post/post-page.tsx
@@ -1,3 +1,29 @@
+import { redirect, useNavigate, useParams } from 'react-router-dom'
+
+import { GoBack } from '@features/go-back'
+
+import { postApi } from '@entities/post'
+import { PostDetails } from '@entities/post/ui/post-details'
+
+import { PATH_PAGE } from '@shared/lib/react-router'
+
 export const PostPage = () => {
-  return <div>Post</div>
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  if (!id || !Number(id)) {
+    return redirect(PATH_PAGE.notFound)
+  }
+
+  const { data: post, isError, isLoading } = postApi.useFetchPostByIdQuery(+id)
+
+  if (isLoading) {
+    return <p>Загрузка...</p>
+  }
+
+  if (isError || !post) {
+    return navigate(PATH_PAGE.notFound, { replace: true })
+  }
+
+  return <PostDetails post={post} slot={<GoBack />} />
 }
