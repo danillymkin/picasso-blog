3b3bf5a0941f04151f140e5c17c14ca3cb4c5dcb
danillyamkin <danilwebdev@gmail.com>
2023-10-11 19:58:50 +0300

    fix: remove listeners

--- src/app/providers/store.tsx
+++ src/app/providers/store.tsx
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query'
 
 import { postApi } from '@entities/post'
 
@@ -10,5 +9,3 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(postApi.middleware),
 })
-
-setupListeners(store.dispatch)
