3ef0c037bb04dcdedc58920a46a84cff60ec6078
danillyamkin <danilwebdev@gmail.com>
2023-10-11 20:40:51 +0300

    refactor: improve rtk query api

--- src/shared/api/base-query.ts
+++
@@ -1,5 +0,0 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-export const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://jsonplaceholder.typicode.com',
-})
--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,1 +1,9 @@
-export { baseQuery } from './base-query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://jsonplaceholder.typicode.com',
+  }),
+  endpoints: () => ({}),
+})
